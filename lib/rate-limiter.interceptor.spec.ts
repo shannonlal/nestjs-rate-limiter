cc46818b5c1ae2d0ae809e44247f2a64e302bdf0
Shannon Lal <shannonlal@hotmail.com>
2020-11-28 13:00:47 -0500

    Check in first test for intereptor

---
+++ lib/rate-limiter.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { RateLimiterInterceptor } from './rate-limiter.interceptor'
+
+describe('RateLimiterInterceptor', () => {
+	it('should validate that RateLimiterInterceptor class exists', async () => {
+		expect(RateLimiterInterceptor).toBeDefined()
+	})
+})
