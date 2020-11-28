f809bd3029ce3cf8830caddfc8fe70a20ece160f
Shannon Lal <shannonlal@hotmail.com>
2020-11-28 13:21:25 -0500

    Check in latest test cases

--- lib/rate-limiter.interceptor.spec.ts
+++ lib/rate-limiter.interceptor.spec.ts
@@ -1,22 +1,39 @@
 import { Reflector } from '@nestjs/core'
 jest.mock('@nestjs/core')
+
+import { NestInterceptor, Injectable, ExecutionContext, CallHandler, Inject, HttpStatus, Logger, HttpException } from '@nestjs/common'
 import { RateLimiterInterceptor } from './rate-limiter.interceptor'
 import { RateLimiterOptions } from './rate-limiter.interface'
 import { defaultRateLimiterOptions } from './default-options'
 
 describe('RateLimiterInterceptor', () => {
-	it('should validate that RateLimiterInterceptor class exists', async () => {
-		expect(RateLimiterInterceptor).toBeDefined()
+
+	describe('Constructor', ()=> {
+		it('should validate that RateLimiterInterceptor class exists', async () => {
+			expect(RateLimiterInterceptor).toBeDefined()
+		})
+	
+		it('should create the RateLimiterInterceptor' , async () => {
+			
+			const rateLimiterOptions: RateLimiterOptions = defaultRateLimiterOptions;
+			const reflector = new Reflector();
+	
+			const interceptor:RateLimiterInterceptor = new RateLimiterInterceptor(rateLimiterOptions,reflector );
+	
+			expect( interceptor ).toBeDefined();
+			
+		})
 	})
 
-	it('should create the RateLimiterInterceptor' , async () => {
-		
-		const rateLimiterOptions: RateLimiterOptions = defaultRateLimiterOptions;
-		const reflector = new Reflector();
+	describe('intercept', ()=> {
+		it('should validate that intercept method exists', async () => {
+			const rateLimiterOptions: RateLimiterOptions = defaultRateLimiterOptions;
+			const reflector = new Reflector();
+	
+			const interceptor:RateLimiterInterceptor = new RateLimiterInterceptor(rateLimiterOptions,reflector );
+	
+			expect( interceptor.intercept ).toBeDefined();
+		})
 
-		const interceptor:RateLimiterInterceptor = new RateLimiterInterceptor(rateLimiterOptions,reflector );
-
-		expect( interceptor ).toBeDefined();
-		
 	})
 })
